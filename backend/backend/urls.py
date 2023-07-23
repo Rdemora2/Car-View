7dd5bf7a3b85689a440fe8832fa1aab0a6bfae1b
Rdemora2 <robertomoraeszar@gmail.com>
2023-07-23 01:11:23 -0300

    cria rota para imagem do veiculo

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from veiculos.views import VeiculoListCreateView, VeiculoDetailView
+from veiculos.views import VeiculoListCreateView, VeiculoDetailView, serve_imagem
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/veiculos/', VeiculoListCreateView.as_view(), name='veiculo_list_create'),
     path('api/veiculos/<int:pk>/', VeiculoDetailView.as_view(), name='veiculo_detail'),
+    path('api/imagens/<str:filename>/', serve_imagem, name='serve-imagem'),
 ]
--- backend/veiculos/views.py
+++ backend/veiculos/views.py
@@ -1,3 +1,6 @@
+import os
+from django.conf import settings
+from django.http import HttpResponse, Http404
 from rest_framework import generics
 from .models import Veiculo
 from .serializers import VeiculoSerializer
@@ -10,3 +13,11 @@ class VeiculoListCreateView(generics.ListCreateAPIView):
 class VeiculoDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Veiculo.objects.all()
     serializer_class = VeiculoSerializer
+
+def serve_imagem(request, filename):
+    imagem_path = os.path.join(settings.STATICFILES_DIRS[0], filename)
+    if os.path.exists(imagem_path):
+        with open(imagem_path, 'rb') as f:
+            return HttpResponse(f.read(), content_type='image/jpeg')
+    else:
+        raise Http404
