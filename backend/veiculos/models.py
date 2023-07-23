62fc59a747414af6f94f93d66351364f8230cb98
Rdemora2 <robertomoraeszar@gmail.com>
2023-07-22 22:19:47 -0300

    configura models do veiculo

--- backend/veiculos/models.py
+++ backend/veiculos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Veiculo(models.Model):
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100)
+    foto = models.ImageField(upload_to='veiculos/static/')
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.marca} {self.modelo}"
