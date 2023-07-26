e98c4a8441b9a68b9a93941a1e7e241f5a03ff72
Rdemora2 <robertomoraeszar@gmail.com>
2023-07-26 13:22:49 -0300

    add msg de erro ao criar veiculo

--- frontend/src/app/veiculo-create/veiculo-create.component.ts
+++ frontend/src/app/veiculo-create/veiculo-create.component.ts
@@ -1,31 +1,31 @@
 import { Component } from '@angular/core';
-import { Veiculo } from '../services/veiculo';
 import { VeiculoService } from '../services/veiculo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-veiculo-create',
   templateUrl: './veiculo-create.component.html',
   styleUrls: ['./veiculo-create.component.css']
 })
+
 export class VeiculoCreateComponent {
   novoVeiculo = {
     marca: '',
     modelo: '',
-    foto: null as File | null, // Inicialize como File | null
+    foto: null as File | null,
     valor: 0
   };
 
-  constructor(private veiculoService: VeiculoService) {}
+  constructor(private veiculoService: VeiculoService, private router: Router) {}
 
   onFotoSelecionada(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length) {
-      this.novoVeiculo.foto = input.files[0]; // Atribua o arquivo selecionado à propriedade "foto"
+      this.novoVeiculo.foto = input.files[0];
     }
   }
 
   criarVeiculo(): void {
-    // Crie um novo objeto FormData e adicione os campos do novoVeiculo a ele
     const formData = new FormData();
     formData.append('marca', this.novoVeiculo.marca);
     formData.append('modelo', this.novoVeiculo.modelo);
@@ -34,19 +34,20 @@ export class VeiculoCreateComponent {
       formData.append('foto', this.novoVeiculo.foto, this.novoVeiculo.foto.name);
     }
 
-    // Chame o novo método do serviço para criar o veículo com a foto, passando o FormData
     this.veiculoService.criarVeiculoComFoto(formData).subscribe(
       () => {
-        // Limpar o formulário após criar o veículo
         this.novoVeiculo = {
           marca: '',
           modelo: '',
-          foto: null, // Defina como null após criar o veículo
+          foto: null,
           valor: 0
         };
+        // Redireciona para o catalogo de veículos
+        this.router.navigate(['/catalog']);
+
       },
       (error) => {
-        // Lide com o erro, se necessário
+        console.error('Erro ao criar veículo:', error);
       }
     );
   }
